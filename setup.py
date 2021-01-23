b66078c00f483d34aedfe2c0297fb9554d8b8b1a
render1980 <render1980@gmail.com>
2021-01-24 01:47:44 +0300

    + requirements.txt to pkg

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(name='grassroot-assistant',
       version='1.0',
       py_modules=['main', 'redis_client'],
+      data_files=[('config', ['requirements.txt'])],
       url='https://t.me/GrassrootsAssistantBot',
       author='render1980',
       license='MIT',
