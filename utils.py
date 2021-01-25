f31ae483aa54909996ac6442fa69173a284633c7
render1980 <render1980@gmail.com>
2021-01-25 17:53:52 +0300

    - rm unused files

--- utils.py
+++
@@ -1,9 +0,0 @@
-import uuid
-import hashlib
-
-
-def generate_token(group, admin_id):
-    print("generating token for group={} admin_id={}".format(group, admin_id))
-    uid = uuid.uuid4()
-    hash = hashlib.sha256("{}:{}:{}".format(group, admin_id, uid).encode("utf-8"))
-    return hash.hexdigest()
