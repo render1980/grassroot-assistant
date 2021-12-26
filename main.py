4b23fc78cd700ae2a4ca571b937945ff2e963554
render1980 <render1980@gmail.com>
2021-12-26 18:32:48 +0200

    changed proc type; err check

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def process_list_groups(update: telegram.Update, ctx: CallbackContext):
 def list_groups(admin_id, radius):
     """ radius - radius to search groups """
     loc = rds.get_location(admin_id)
-    if loc == 0:
+    if (not loc):
         return "Error: cannot find your current location. Could you, please, send it again.."
     longitude = loc[0]
     latitude = loc[1]
